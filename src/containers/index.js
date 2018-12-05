8c7bab00ba240a99a8c5350957df0a45233ae88e
chnbohwr <chnbohwr@gmail.com>
2018-12-05 22:17:34 +0800

    first commit

---
+++ src/containers/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h2>Hello, I`m ReactMaker.</h2>
+        <p>To get started, edit containers/index.js</p>
+      </div>
+    );
+  }
+}
