bb0cf7c52587e304ec9b9b0d2a5ca6eff640f318
chnbohwr <chnbohwr@gmail.com>
2018-12-13 23:10:36 +0800

    update doc

--- src/index.js
+++ src/index.js
@@ -8,24 +8,36 @@ import Container from './components/Container';
 const Demo = () => (
   <div>
     <FontFace />
-    <Button>Normal</Button>
-    <Button type="primary">Primary</Button>
-    <Button type="success">Success</Button>
-    <Button type="warning">Warning</Button>
-    <Button type="error">Error</Button>
-    <Container>hi this is container</Container>
-    <Container isDark>
-      <div>hi this is dark container</div>
-    </Container>
+    <div>
+      <Button>Normal</Button>
+      <Button type="primary">Primary</Button>
+      <Button type="success">Success</Button>
+      <Button type="warning">Warning</Button>
+      <Button type="error">Error</Button>
+    </div>
+    <br />
+    <Container title="Container">
+      <Container>hi this is container</Container>
+      <br />
+      <Container isDark>
+        <div>hi this is dark container</div>
+      </Container>
+      <br />
+      <Container isDark isRounded>
+        <div>Good morning. Thou hast had a good night's sleep, I hope.</div>
+      </Container>
+      <br />
+      <div style={{ display: 'flex' }}>
+        <Container isRounded style={{ width: 300 }}>
+          <div>Good morning. Thou hast had a good night's sleep, I hope.</div>
+        </Container>
+        <Container isDark isRounded title="Super easy">
+          <div>Good morning. Thou hast had a good night's sleep, I hope.</div>
+        </Container>
+      </div>
 
-    <Container isDark isRounded>
-      <div>Good morning. Thou hast had a good night's sleep, I hope.</div>
-    </Container>
 
-    <Container isRounded style={{width: 300}}>
-      <div>Good morning. Thou hast had a good night's sleep, I hope.</div>
     </Container>
-
   </div>
 );
 
